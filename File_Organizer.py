9f76c2bd96530418a584954a8ff39d37e0d97a52
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-03-17 10:13:20 +0530

    Added file organization logic and function to File_Organizer.py script

--- File_Organizer.py
+++ File_Organizer.py
@@ -21,6 +21,24 @@ def get_file_type(extension):
             return category
     return 'Others'
 
+def organize_files():
+    # Get all files in the folder
+    for file_name in os.listdir(folder_to_organize):
+        file_path = os.path.join(folder_to_organize, file_name)
+
+        if os.path.isfile(file_path):
+            file_extension = os.path.splitext(file_name)[1].lower()
+            category = get_file_type(file_extension)
+            category_folder = os.path.join(folder_to_organize, category)
+            
+            # Create category folder if it doesn't exist
+            os.makedirs(category_folder, exist_ok=True)
+
+            # Move file to respective category folder
+            shutil.move(file_path, os.path.join(category_folder, file_name))
+            print(f'Moved {file_name} to {category}/')
+
 if __name__ == '__main__':
+    organize_files()
     print('Files have been organized!')
   
